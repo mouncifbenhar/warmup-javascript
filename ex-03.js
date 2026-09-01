57f9de83432ca9d8137e94bf762367fc90041ad7
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-09-01 16:34:38 +0100

    ex-03

---
+++ ex-03.js
@@ -0,0 +1,29 @@
+for(let i = 1 ; i <= 100 ; i++){
+    console.log(i)
+    if(i == 10){
+        console.log("Décollage !");
+        return;
+    }
+
+}
+
+let i = 1
+while(i <= 10){
+    console.log(i)
+    i++;
+}
+
+
+
+
+let a = 0
+for(let i = 0 ; i <= 100 ; i++ ){
+    a = a + i ;
+}
+console.log(a);
+
+for(let i = 0 ; i <= 100 ; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
