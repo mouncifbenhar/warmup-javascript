b481b63b48ec0f436ef9763e71f91f1feb8bd985
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-09-02 10:55:37 +0100

    ex-04

--- ex-04.js
+++ ex-04.js
@@ -2,9 +2,20 @@ const courses = ["pain", "lait", "riz", "cafe"];
 
 let a =  courses.length
 console.log(`Nombre d'articles : ${a}`)
+courses.push("sucre")
+console.log(courses.indexOf("cafe"))
+courses.splice(courses.indexOf("cafe"),1)
+courses.forEach(e => {
+    console.log(e)
+});
 
+console.log(courses.indexOf("a"))
 
-
+if(courses.indexOf("cafe") === -1){
+    console.log("no")
+}else{
+    console.log("Le cafe est bien dans la liste.")
+}
 
 
 
