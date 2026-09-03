c557e01fde5e0271d1088013dd822fe83d05c1df
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-09-03 17:13:24 +0100

    feat

--- ex-06.js
+++ ex-06.js
@@ -10,4 +10,29 @@ const panier = [
     console.log(e)
     });
 
-    const p = panier.map
+
+    const p = panier.map(e =>
+    {
+        return {
+            name:  e.nom,
+            total: e.prix * e.quantite
+        }
+    }
+    )
+    console.log(p)
+
+    const panier_prix_bigger_than_100 = panier.filter( e => e.prix > 100).map(e => e.nom)
+
+    console.log(panier_prix_bigger_than_100)
+
+ 
+ function prix_total_of_oune_prodact(prix,quantite){
+        return  prix * quantite
+    }
+    
+const Total_panier = panier.reduce((total , element ) => total + prix_total_of_oune_prodact(element.prix,element.quantite) ,0)
+
+console.log(Total_panier)
+
+const total_prodact = panier.reduce((total, element) => total + element.quantite ,0);
+console.log(total_prodact)
